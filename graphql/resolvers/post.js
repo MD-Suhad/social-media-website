ac566406fe587c4416f99286553c8e977bb7c790
MD-Suhad <ssuhad490@gmail.com>
2023-04-05 23:43:46 +0600

    update project

--- graphql/resolvers/post.js
+++ graphql/resolvers/post.js
@@ -1,4 +1,5 @@
 const Post = require("../../models/Post");
+const checkAuth  = require("../../util/check-auth");
 
 module.exports = {
    Query:{
@@ -30,9 +31,15 @@ module.exports = {
  Mutation:{
    async createPost(_,{body},context){
          const user = checkAuth(context);
+         console.log(user);
          const newPost = new Post({
-            
-         })
+            body,
+            user:user.id,
+            username:user.username,
+            createAt: new Date().toISOString()
+         });
+         const post = await newPost.save();
+         return post;
    }
  }
 }
--- util/check-auth.js
+++ util/check-auth.js
@@ -1,3 +1,6 @@
+const {AuthenticationError} = require('apollo-server');
+
+
 const jwt = require("jwt");
 const { SECRET_KEY } = require("../config");
 
@@ -7,12 +10,14 @@ module.exports = (context) => {
   const authHeader = context.req.header.authorization;
   if (authHeader) {
     const token = authHeader.split("Bearer")[1];
-    try {
-      const user = jwt.verify(token, SECRET_KEY);
-      return user;
-    } catch (err) {
-      throw new AuthenticationError("Invalid/Expired token");
-    }
+   if(token){
+      try {
+         const user = jwt.verify(token, SECRET_KEY);
+         return user;
+       } catch (err) {
+         throw new AuthenticationError("Invalid/Expired token");
+       }
+   }
     throw new Error("Authentication token must be 'Bearer[token]");
   }
   throw new Error("Authentication header must be provided");
